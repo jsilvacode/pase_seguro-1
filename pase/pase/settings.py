44259adbe29b92b398fe7bd17f98d91861f04c54
mena-hub <mssz.nnia@gmail.com>
2021-08-06 11:28:45 -0300

    Remove, models, home

--- pase/pase/settings.py
+++ pase/pase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(i(j$4qsm^tmi5(7nvrfueubjg^iu6xt00p^9g)xd*uj6)9sm)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
