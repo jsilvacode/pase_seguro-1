d258f74fe14280b79edec4e18dbfcf4dc1058b3d
mena-hub <mssz.nnia@gmail.com>
2021-08-06 20:55:19 -0300

    models II

--- pase/core/admin.py
+++ pase/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (AntecedentesAcademicos, ActividadAcademica,
+                     ActividadGeneral, AntecedentesPersonales, AntecedentesSanitarios,
+                     Estudiante
+                    )
 
-# Register your models here.
+admin.site.register(AntecedentesAcademicos)
+admin.site.register(ActividadAcademica)
+admin.site.register(ActividadGeneral)
+admin.site.register(AntecedentesPersonales)
+admin.site.register(AntecedentesSanitarios)
+admin.site.register(Estudiante)
--- pase/core/models.py
+++ pase/core/models.py
@@ -1,21 +1,48 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
-from django.db.models.fields.related import ForeignKey
+
+
+class AntecedentesPersonales(models.Model):
+    rut = models.CharField(max_length=12, unique=True, verbose_name="RUT")
+    nombres = models.CharField(max_length=200)
+    apellidos = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100, verbose_name="correo electrónico")
+    telefono = models.CharField(max_length=20, verbose_name="teléfono fijo / celular")
+    
+    def __str__(self):
+        return self.rut
+
+    class Meta:
+        verbose_name = "Antecedente Personal"
+        verbose_name_plural = "Antecedentes Personales"
+
+
+class AntecedentesAcademicos(models.Model):
+    facultad = models.CharField(max_length=100)
+    carrera = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{} - {}'.format(self.facultad, self.carrera)
+
+    class Meta:
+        verbose_name = "Antecedente Académico"
+        verbose_name_plural = "Antecedentes Académicos"
 
 
 class ActividadAcademica(models.Model):
     fecha = models.DateField()
-    hora_inicio = models.DateField()
-    hora_fin = models.DateField()
-    aula = models.CharField()
-    asignatura = models.CharField()
-    docente = models.CharField()
-    facultad = models.CharField()
-    carrera = models.CharField()
+    hora_inicio = models.TimeField(verbose_name="horario de inicio")
+    hora_fin = models.TimeField(verbose_name="horario de término")
+    aula = models.CharField(max_length=100, verbose_name="aula / dependencia")
+    asignatura = models.CharField(max_length=100)
+    docente = models.CharField(max_length=200)
+    facultad_carrera = models.ManyToManyField(
+        AntecedentesAcademicos, related_name="get_faculty_career",
+        verbose_name="facultad / carrera")
 
     class Meta:
-        verbose_name = 'Actividad Academica'
-        verbose_name_plural = 'Actividades Académicas'
+        verbose_name = "Actividad Académica"
+        verbose_name_plural = "Actividades Académicas"
 
     def __str__(self):
         return '{} - {}'.format(self.asignatura, self.docente)
@@ -23,81 +50,100 @@ class ActividadAcademica(models.Model):
 
 class ActividadGeneral(models.Model):
     fecha = models.DateField()
-    hora_inicio = models.DateField()
-    hora_fin = models.DateField()
-    dependencia = models.CharField()
-    descripcion = models.TextField()
+    hora_inicio = models.TimeField(verbose_name="horario de inicio")
+    hora_fin = models.TimeField(verbose_name="horario de término")
+    dependencia = models.CharField(max_length=100, verbose_name="aula / dependencia")
+    descripcion = models.TextField(verbose_name="descripción / comentario")
 
     def __str__(self):
         pass
 
     class Meta:
-        verbose_name = 'Actividad General'
-        verbose_name_plural = 'Actividades Generales'
+        verbose_name = "Actividad General"
+        verbose_name_plural = "Actividades Generales"
 
 
-class AntecedenteAcademico(models.Model):
-    facultad = models.CharField()
-    carrera = models.CharField()
+class AntecedentesSanitarios(models.Model):
+    # COVID-19
+    CONTACTO_ESTRECHO_OPCIONES = (('sí', 'Sí'), ('no', 'No'))
+    contacto_estrecho = models.CharField(
+        max_length=10, choices=CONTACTO_ESTRECHO_OPCIONES, help_text="¿Ha tenido?."
+    )
 
-    def __str__(self):
-        return '{} - {}'.format(self.facultad, self.carrera)
+    # Antecedentes de salud I
+    enfermedad_autoinmune = models.CharField(
+        max_length=100, blank=True, verbose_name="enfermedad autoinmune"
+    )
+    enfermedad_cronica = models.CharField(
+        max_length=100, blank=True, verbose_name="enfermedad crónica"
+    )
 
-    class Meta:
-        verbose_name = 'Antecedente Académico'
-        verbose_name_plural = 'Antecedentes Académicos'
+    # Síntomas cardinales
+    sintoma_fiebre = models.BooleanField(verbose_name="fiebre")
+    sintoma_perdida_olfato = models.BooleanField(verbose_name="pérdida del olfato")
+    sintoma_perdida_gusto = models.BooleanField(verbose_name="pérdida del gusto")
 
+    # Síntomas no cardinales
+    sintoma_tos = models.BooleanField(verbose_name="tos")
+    sintoma_dolor_garganta = models.BooleanField(verbose_name="dolor de garganta")
+    sintoma_secrecion_nasal = models.BooleanField(verbose_name="secreciones nasales")
+    sintoma_respiratorio = models.BooleanField(verbose_name="dificultad respiratoria")
+    sintoma_aumento_frecuencia = models.BooleanField(
+        verbose_name="aumento de la frecuencia respiratoria"
+    )
+    sintoma_dolor_toracico = models.BooleanField(verbose_name="dolor torácico")
+    sintoma_dolor_muscular = models.BooleanField(verbose_name="dolor muscular")
+    sintoma_dolor_cabeza = models.BooleanField(verbose_name="dolor de cabeza")
+    sintoma_fatiga = models.BooleanField(verbose_name="fatiga")
+    sintoma_escalofrios = models.BooleanField(verbose_name="escalofríos")
+    sintoma_diarrea = models.BooleanField(verbose_name="diarrea")
+    sintoma_nausea_vomitos = models.BooleanField(verbose_name="náusea / vómitos")
 
-class AntecedentesPersonales(models.Model):
-    rut = models.CharField()
-    nombres = models.CharField()
-    apellidos = models.CharField()
-    email = models.EmailField()
-    telefono = models.CharField()
-    
-    def __str__(self):
-        return self.rut
+    # Otros 
+    sintoma_otro = models.CharField(max_length=200, blank=True, verbose_name="otros síntomas")
 
-    class Meta:
-        verbose_name = 'Antecedente Sanitario'
-        verbose_name_plural = 'Antecedentes Sanitarios'
-
-
-class AntecedenteSanitario(models.Model):
-    contacto_estrecho = models.BooleanField()
-    sintomas_cardinales = models.BooleanField()
-    sintomas_no_cardinales = models.BooleanField()
-    declaracion_salud = models.FileField()
+    # Antecedentes de salud II
+    EMBARAZADA_OPCIONES = (('sí', 'Si'), ('no', 'No'))
+    embarazo = models.CharField(
+        max_length=10, blank=True, choices=EMBARAZADA_OPCIONES, help_text="¿Está?."
+    )
+    declaracion_confirmar = models.BooleanField(
+        default=False, verbose_name="declaración de salud idónea", 
+        help_text="Aceptar."
+    )
 
     def __str__(self):
         pass
 
     class Meta:
-        verbose_name = 'Antecedente Sanitario'
-        verbose_name_plural = 'Antecedentes Sanitarios'
+        verbose_name = "Antecedente Sanitario"
+        verbose_name_plural = "Antecedentes Sanitarios"
 
 
 class Estudiante(models.Model):
     antecedente_personal = models.ForeignKey(
-        'AntecedentesPersonales', verbose_name='antecedentes personales',
-        null=False, blank=False, on_delete=models.CASCADE)
-    antecedente_academico = models.ForeignKey(
-        'AntecedenteAcademico', on_delete=CASCADE,
-        null=False, blank=False, verbose_name='Antecedente Academico'
+        AntecedentesPersonales, on_delete=models.CASCADE, null=False, blank=False,
+        verbose_name="antecedentes personales",
     )
+    antecedente_academico = models.ForeignKey(
+        AntecedentesAcademicos, on_delete=CASCADE, null=False, blank=False, 
+        verbose_name="antecedentes académicos"),
     actividad_academica = models.ForeignKey(
-        'ActividadAcademica', verbose_name='Actividad Academica',
-        null=False, blank=False, on_delete=CASCADE
+        ActividadAcademica, on_delete=CASCADE, null=False, blank=False,
+        verbose_name="actividades académicas", help_text="Eventos."
     )
     antecedente_sanitario = models.ForeignKey(
-        'AntecedenteSanitario', verbose_name='Antecedente Sanitario',
-        null=False, blank=False, on_delete=CASCADE
+        AntecedentesSanitarios, on_delete=CASCADE, null=False, blank=False, 
+        verbose_name="antecedentes sanitarios",
     )
-    declaracion_salud = models.FileField()
+    declaracion_archivo = models.FileField(
+        upload_to='uploads/responsibilities/%Y/%m/%d/', blank=True, 
+        verbose_name="declaración de responsabilidad", help_text="Doc adjunto."
+    )
 
     def __str__(self):
         pass
 
     class Meta:
-        verbose_name = 'Estudiante'
-        verbose_name_plural = 'Estudiantes'
+        verbose_name = "Estudiante"
+        verbose_name_plural = "Estudiantes"
