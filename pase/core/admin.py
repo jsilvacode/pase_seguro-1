35a198a91d2893d054410391f6aff76af6f59159
Emi <emazzurque@gmail.com>
2021-08-07 15:45:02 -0300

    add import-export

--- pase/core/admin.py
+++ pase/core/admin.py
@@ -1,11 +1,24 @@
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from django.contrib import admin
 from .models import (AntecedentesAcademicos, ActividadAcademica,
                      ActividadGeneral, AntecedentesPersonales, AntecedentesSanitarios,
                      Estudiante
                     )
 
+
+class ActividadAcademicaResource(resources.ModelResource):
+
+    class Meta:
+        model = ActividadAcademica
+
+
+class ActividadAcademicaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = ActividadAcademicaResource
+
 admin.site.register(AntecedentesAcademicos)
-admin.site.register(ActividadAcademica)
+admin.site.register(ActividadAcademica, ActividadAcademicaAdmin)
 admin.site.register(ActividadGeneral)
 admin.site.register(AntecedentesPersonales)
 admin.site.register(AntecedentesSanitarios)
--- pase/pase/settings.py
+++ pase/pase/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'safepass',
+    'import_export',
 ]
 
 MIDDLEWARE = [
