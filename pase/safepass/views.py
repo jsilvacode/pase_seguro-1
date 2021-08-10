7c0de17c7cde4c58b413f556024dbca90549309e
Emi <emazzurque@gmail.com>
2021-08-10 11:08:31 -0300

    add advance

--- pase/safepass/views.py
+++ pase/safepass/views.py
@@ -1,29 +1,45 @@
 from django.http.response import Http404
 from django.shortcuts import render
-from .models import AntecedentesPersonales
+from .models import AntecedentesPersonales, ActividadAcademica, Visita
+from datetime import date
+
 
 
 def check(request):
+    algo = ""
     return render(request, "safepass/check.html")
 
 
+""" def check_form(request):
+    algo = ""
+    return render(request, "safepass/check_form.html") """
+
+
+
 def check_form(request):
-    return render(request, "safepass/check_form.html")
-
-
-"""
-def check_form(request, student_id):
     try:
-        personal_fields = AntecedentesPersonales.objects.get(id=student_id)
+        if request.POST:
+            a=request.POST['rut']
+            id_visit = AntecedentesPersonales.objects.get(rut=a).id
+            personal_fields = Visita.objects.filter(antecedente_personal_id=id_visit)
     except AntecedentesPersonales.DoesNotExist:
         raise Http404("Estudiante no registrado")
     return render(request, "safepass/check_form.html", {'personal_fields': personal_fields})
-"""
+
 
 
 def students_form(request):
+    today = date.today()
+    all = ActividadAcademica.objects.all()
+    for x in all:
+        if x.fecha > today:
+            print(x.fecha)
+            print(x.hora_inicio)
+            print(x.aula)
+            print(x.docente)
 
-    return render(request, "safepass/students.html")
+    contexto = {}
+    return render(request, "safepass/students.html",{"contexto":contexto})
 
 
 def visitors_form(request):
